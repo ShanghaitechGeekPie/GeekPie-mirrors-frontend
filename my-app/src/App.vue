90b1167e377d1328fb6461d1c17fe029bdcbb25a
nyovelt <qinfr@shanghaitech.edu.cn>
2020-08-25 22:42:21 +0800

    sidebar

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,78 +1,71 @@
 <template>
-    <v-app class="overflow-hidden">
-        <v-app-bar
-                app
-                fixed
-                color="#fcb69f"
-                dark
-                shrink-on-scroll
-                src="https://picsum.photos/1920/1080?random"
-                height="200"
-        >
-            <div class="d-flex align-center">
-                <v-img
-                        alt="Logo"
-                        class="shrink mr-2"
-                        contain
-                        src="logo.png"
-                        transition="scale-transition"
-                        height="50"/>
-            </div>
-            <v-toolbar-title style="font-size: 125%"
-            >GeekPie Open Source Mirror
-            </v-toolbar-title>
-        </v-app-bar>
-
-
-        <v-sheet
-                id="scrolling-techniques"
-                class="main-container"
-                style="max-height: calc(100vh - 64px);"
-        >
-            <v-container>
-                <v-main>
-                    <v-row>
-                        <v-col
-                                class="d-flex"
-                                cols="9">
-                            <test/>
-
-                        </v-col>
-                    </v-row>
-
-
-                </v-main>
-            </v-container>
-        </v-sheet>
-    <v-footer
-        color=indigo
+  <v-app class="overflow-hidden">
+    <v-app-bar
+      app
+      fixed
+      color="#fcb69f"
+      dark
+      shrink-on-scroll
+      src="https://picsum.photos/1920/1080?random"
+      height="200"
     >
-        <span class="white--text">Geek Pie Association &copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-    </v-app>
-    
+      <div class="d-flex align-center">
+        <v-img
+          alt="Logo"
+          class="shrink mr-2"
+          contain
+          src="logo.png"
+          transition="scale-transition"
+          height="50"
+        />
+      </div>
+      <v-toolbar-title style="font-size: 125%">GeekPie Open Source Mirror</v-toolbar-title>
+    </v-app-bar>
 
+    <v-sheet
+      id="scrolling-techniques"
+      class="main-container"
+      style="max-height: calc(100vh - 64px);"
+    >
+      <v-container>
+        <v-main>
+          <v-row>
+            <!-- <v-col class="d-flex" cols="9">
+              <test />
+            </v-col>-->
 
+            <div class="d-flex col col-9">
+                <sidebar />
+            </div>
+          </v-row>
+        </v-main>
+      </v-container>
+    </v-sheet>
+    <!-- <v-footer color="indigo">
+      <span class="white--text">Geek Pie Association &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>-->
+  </v-app>
 </template>
 
 <script>
-    // import HelloWorld from './components/HelloWorld';
-    // import news from './components/news';
-    import test from './components/test';
-    export default {
-        name: 'App',
+// import HelloWorld from './components/HelloWorld';
+// import news from './components/news';
+import sidebar from "./components/sidebar";
+export default {
+  name: "App",
 
-        components: {
-            test
-        },
+  components: {
+    sidebar,
+  },
 
-        data: () => ({
-            windowHeight: document.documentElement.clientHeight - 1000
-        }),
-        computed: {
-            scrollerHeight: function () {
-                return (window.innerHeight) * 0.9;
-            }
-        }
-    };
+  data: () => ({
+    windowHeight: document.documentElement.clientHeight - 1000,
+  }),
+
+  computed: {
+    scrollerHeight: function () {
+      return window.innerHeight * 0.9;
+    },
+  },
+};
 </script>
---
+++ my-app/src/components/sidebar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col col-12">
+        <v-card class="mx-auto" max-width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">镜像源状态</p>
+            <!-- <p>adjective</p> -->
+            <div class="text--primary">
+              一切正常
+              <br />all systems green
+            </div>
+          </v-card-text>
+          <!-- <v-card-actions>
+                        <v-btn text color="deep-purple accent-4">Learn More</v-btn>
+          </v-card-actions>-->
+        </v-card>
+      </div>
+
+      <div class="col col-12">
+        <v-card class="mx-auto" max-width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">镜像源地址</p>
+            <!-- <p>adjective</p> -->
+
+            <div class="text--primary">
+              <a href="https://mirrors.geekpie.club/">mirrors.geekpie.club</a> 自动切换
+              <br />
+              <a href="https://mirrors-lan.geekpie.club">mirrors-lan.geekpie.club</a> 校内
+              <br />
+              <a href="https://mirrors-wan.geekpie.club">mirrors-wan.geekpie.club</a> 校外
+            </div>
+          </v-card-text>
+        </v-card>
+      </div>
+
+      <div class="col col-12">
+        <v-card class="mx-auto" max-width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">镜像源文档</p>
+            <!-- <p>adjective</p> -->
+            <div class="text--primary">如果您不了解如何配置和使用源， 这里为您提供了相关文档以供查阅：</div>
+          </v-card-text>
+
+          <div class="text-center">
+            <v-btn rounded color="primary" href="https://mirrors.geekpie.club/help/" dark>镜像源文档</v-btn>
+          </div>
+          <br />
+        </v-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+};
+</script>
