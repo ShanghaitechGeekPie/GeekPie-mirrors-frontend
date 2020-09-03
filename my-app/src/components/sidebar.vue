6ed784d0b74d835650cbc7fe3f2e5205c467d40c
tt <t@t.tt>
2020-09-03 15:02:21 +0800

    what does the LUG means

--- my-app/src/components/sidebar.vue
+++ my-app/src/components/sidebar.vue
@@ -8,7 +8,7 @@
             <!-- <p>adjective</p> -->
             <div class="text--primary">
               一切正常
-              <br />all systems green
+              <br/>all systems green
             </div>
           </v-card-text>
           <!-- <v-card-actions>
@@ -25,9 +25,9 @@
 
             <div class="text--primary">
               <a href="https://mirrors.geekpie.club/">mirrors.geekpie.club</a> 自动切换
-              <br />
+              <br/>
               <a href="https://mirrors-lan.geekpie.club">mirrors-lan.geekpie.club</a> 校内
-              <br />
+              <br/>
               <a href="https://mirrors-wan.geekpie.club">mirrors-wan.geekpie.club</a> 校外
             </div>
           </v-card-text>
@@ -45,7 +45,7 @@
           <div class="text-center">
             <v-btn rounded color="primary" href="https://mirrors.geekpie.club/help/" dark>镜像源文档</v-btn>
           </div>
-          <br />
+          <br/>
         </v-card>
       </div>
 
@@ -55,12 +55,50 @@
             <p class="display-1 text--primary">其他链接</p>
           </v-card-text>
           <a
-            v-for="(link, i) in otherlinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
+              v-for="(link, i) in otherlinks"
+              :key="i"
+              :href="link.href"
+              class="subheading mx-3"
+              target="_blank"
           >{{ link.text }}<br></a>
+
+        </v-card>
+      </div>
+      <div class="col col-12">
+        <v-card class="mx-auto" max-width="344">
+          <div>
+
+            <v-card-text>
+              <v-progress-circular
+                  indeterminate
+                  color="yellow"
+              >U
+              </v-progress-circular>
+              ： 正在同步
+            </v-card-text>
+          </div>
+          <div>
+
+            <v-card-text>
+              <v-progress-circular
+                  indeterminate
+                  color="primary"
+              >L
+              </v-progress-circular>
+              ： 正在重试
+            </v-card-text>
+          </div>
+          <div>
+
+            <v-card-text>
+              <v-progress-circular
+                  :value="100"
+                  color="green"
+              >G
+              </v-progress-circular>
+              ： 一切正常
+            </v-card-text>
+          </div>
 
         </v-card>
       </div>
