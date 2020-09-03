2cf0e9f55a9e780f72cc6ed66518f494e9f7d45c
tt <t@t.tt>
2020-09-03 22:51:46 +0800

    add status in sidebar

--- my-app/src/components/sidebar.vue
+++ my-app/src/components/sidebar.vue
@@ -103,6 +103,11 @@
 
         </v-card>
       </div>
+      <div  class="col col-12">
+        <v-card class="mx-auto" max-width="344">
+          <v-card-text><span :style="{'color':rgb}">同步状态（平均时间差）： {{getUpdateTime(alltime)}}</span></v-card-text>
+        </v-card>
+      </div>
     </div>
   </div>
 </template>
@@ -111,8 +116,8 @@
 const axios = require('axios');
 export default {
   name: "sidebar",
-  data (){
-    this.otherlinks= [
+  data() {
+    this.otherlinks = [
       {
         text: "镜像源日志 / News",
         href: "https://t.me/GeekPie_mirrors",
@@ -142,9 +147,55 @@ export default {
       url: "Mirrors-Status.json",
       method: 'get',
     }).then(res => {
-      this.repotable = res.data;
+      this.alltime = 0;
+      res.data.forEach((item)=>{this.alltime  += (new Date().getTime() - Math.floor(item.time * 1000));})
+      this.alltime/=32;
+      var percent = Math.log(21600)/Math.log(this.alltime);
+      percent = Math.max(100,percent);
+      percent = Math.min(0,percent);
+      var r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+      var g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+      this.rgb = 'rgb('+r+','+g+',0)';
       this.$forceUpdate();
     })
   },
+  methods: {
+    getUpdateTime: function(updateTime){
+      if (updateTime === null) {
+        return '';
+      }
+      let second = Math.floor((updateTime) / (1000))
+      let minute = Math.floor(second / 60)
+      let hour = Math.floor(minute / 60)
+      let day = Math.floor(hour / 24)
+      let month = Math.floor(day / 31)
+      let year = Math.floor(month / 12)
+      if (year > 0) {
+        return year + '年前';
+      } else if (month > 0) {
+        return month + '月前';
+      } else if (day > 0) {
+        let ret = day + '天前'
+        if (day >= 7 && day < 14) {
+          ret = '1周前'
+        } else if (day >= 14 && day < 21) {
+          ret = '2周前'
+        } else if (day >= 21 && day < 28) {
+          ret = '3周前'
+        } else if (day >= 28 && day < 31) {
+          ret = '4周前'
+        }
+        return ret;
+      } else if (hour > 0) {
+        return hour + '小时前';
+      } else if (minute > 0) {
+        return minute + '分钟前';
+      } else if (second > 0) {
+        return second + '秒前';
+      } else {
+        return '刚刚';
+      }
+    }
+  }
 };
 </script>
